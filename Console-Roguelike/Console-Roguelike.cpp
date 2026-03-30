c801d51f044e3bc3619a54cca21f6c27a71d9dbf
zefruuu <ivanvorobiov@gmail.com>
2026-03-30 22:51:40 +0300

    feat: add function random enemy generation

--- Console-Roguelike/Console-Roguelike.cpp
+++ Console-Roguelike/Console-Roguelike.cpp
@@ -2,19 +2,15 @@
 #include <fstream>
 #include <windows.h>
 #include <conio.h>
+#include <ctime>
 
 
 using namespace std;
 
-const int width = 20;
-const int height = 7;
-
-char map[15][7];
-char background = '~';
-
 struct Point {
 	int x; int y;
 };
+
 
 enum Type {
 	Player,
@@ -36,7 +32,7 @@ enum ItemType {
 struct Entity {
 	char symbol;
 	Point position;
-    Type entityType;
+	Type entityType;
 
 	//enemies
 	int hp_enemy;
@@ -85,7 +81,7 @@ Entity createGuard(int x, int y) {
 Entity createPlayer(int x, int y) {
 	Entity player;
 	player.symbol = 'P';
-	player.position = {x,y};
+	player.position = { x,y };
 	player.entityType = Type::Player;
 	player.hp_player = 20;
 	return player;
@@ -110,16 +106,98 @@ Entity createDB(int x, int y) {
 }
 
 
+
+
+
+const int MAX_GOBLINS = 3;
+int goblins_count = 0;
+Entity goblins[MAX_GOBLINS];
+
+const int MAX_GUARDS = 2;
+int guards_count = 0;
+Entity guards[MAX_GUARDS];
+
+const int MAX_GIGANTS = 2;
+int gigants_count = 0;
+Entity gigants[MAX_GIGANTS];
+
+
+
+const int width = 20;
+const int height = 7;
+
+char map[width][height];
+char background = '~';
+
+
+
+void spawnGoblin() {
+	goblins_count = 1 + rand() % MAX_GOBLINS;
+
+	for (int i = 0; i < goblins_count; i++) {
+		goblins[i] = createGoblin(4 + rand() % (width - 4 + 1), rand() % height);
+
+	}
+
+}
+
+void spawnGuard() {
+	guards_count = 1 + rand() % 2;
+
+	for (int i = 0; i < guards_count; i++) {
+		guards[i] = createGuard(4 + rand() % (width - 4 + 1), rand() % height);
+
+	}
+}
+
+void spawnGigant() {
+	gigants_count = 1 + rand() % 2;
+
+	for (int i = 0; i < gigants_count; i++) {
+		gigants[i] = createGigant(4 + rand() % (width - 4 + 1), rand() % height);
+
+	}
+
+}
+
+
 void draw(Entity player) {
 	for (int y = 0; y < height; y++) {
 		for (int x = 0; x < width; x++) {
-			if (x == player.position.x && y == player.position.y)
-				cout << player.symbol;
-			else {
-				map[x][y] = background;
-				cout << map[x][y];
 
+			char toDraw = background;
+
+			if (x == player.position.x && y == player.position.y) {
+				toDraw = player.symbol;
 			}
+			else {
+				
+				for (int i = 0; i < goblins_count; i++) {
+					if (x == goblins[i].position.x && y == goblins[i].position.y) {
+						toDraw = goblins[i].symbol;
+						break;
+					}
+				}
+
+				
+				for (int i = 0; i < guards_count; i++) {
+					if (x == guards[i].position.x && y == guards[i].position.y) {
+						toDraw = guards[i].symbol;
+						break;
+					}
+				}
+
+				
+				for (int i = 0; i < gigants_count; i++) {
+					if (x == gigants[i].position.x && y == gigants[i].position.y) {
+						toDraw = gigants[i].symbol;
+						break;
+					}
+				}
+			}
+
+			map[x][y] = toDraw;
+			cout << map[x][y];
 		}
 		cout << endl;
 	}
@@ -130,9 +208,11 @@ void playerMove(Entity& player) {
 		switch (_getch()) {
 		case 'a': player.position.x--; break;
 		case 'd': player.position.x++; break;
-		case 'w': player.position.y--; break;
+		case 'w' && 'ö': player.position.y--; break;
 		case 's': player.position.y++; break;
 		}
+	}
+
 	if (player.position.x >= width) {
 		player.position.x--;
 	}
@@ -140,31 +220,38 @@ void playerMove(Entity& player) {
 		player.position.x++;
 	}
 	if (player.position.y >= height) {
-		player.position.y = player.position.y--;
+		player.position.y--;
 	}
 	if (player.position.y < 0) {
-		player.position.y = player.position.y++;
+		player.position.y++;
 	}
-	}
-	Sleep(50);
+	::Sleep(50);
 
 }
 
-/*void drawPlayer(Entity player) {
-	map[player.position.x][player.position.y] = player.symbol;
-	cout << map[player.position.x][player.position.y];
-}*/
-
 int main() {
+	srand(time(NULL));
 	Entity player = createPlayer(0, 3);
+
+	
+	spawnGuard();
+
+	spawnGoblin();
+
+	spawnGigant();
+	
 	bool isRuning = true;
 
 	while (isRuning) {
 		system("cls");
+
 		draw(player);
+
 		playerMove(player);
-
-
+		
+		
+		
+		
 	}
 
 	
